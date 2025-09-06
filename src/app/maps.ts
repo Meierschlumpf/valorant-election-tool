67ceb98c41e989738bb2c66b6b597cca26ddd9c3
Meier Lukas <meierschlumpf@gmail.com>
2025-09-06 22:20:01 +0200

    feat: add corrode map

--- src/app/maps.ts
+++ src/app/maps.ts
@@ -1,2 +1,10 @@
-export const valorantMaps = ["sunset", "lotus" /*"pearl",*/ /*"fracture"*/ /*"breeze"*/, "icebox", "abyss", "bind", "haven" /*"split"*/, "ascent"] as const;
+export const valorantMaps = [
+  "sunset",
+  "lotus" /*"pearl",*/ /*"fracture"*/ /*"breeze"*/,
+  "icebox",
+  /*"abyss",*/ "bind",
+  "haven" /*"split"*/,
+  "ascent",
+  "corrode",
+] as const;
 export type ValorantMap = (typeof valorantMaps)[number];
